76f1c7c70be3e2b51b9fbfd2f09ef78100b07b29
Dragoş <edragos33@gmail.com>
2025-02-04 11:56:28 +0100

    Comments added inside main

--- src/Main.java
+++ src/Main.java
@@ -5,6 +5,7 @@ import javax.management.Notification;
 public class Main {
     public static void main(String[] args) {
 
+        //Les commandes -- Builder
         Order order = new OrderBuilder(1)
                 .addProduct("Computer screen", 400)
                 .addProduct("Mouse", 20)
@@ -14,11 +15,12 @@ public class Main {
 
         System.out.println(order);
 
+        // Mode de payement -- Factory
         IPaymentMethod paymentMethod = FPaymentMethod.getPaymentMethod(EPaymentMethod.CREDIT_CARD);
         System.out.println(paymentMethod);
-
         paymentMethod.pay(order.getTotalPrice());
 
+        // Les notifications -- Observer
         ObserverOrder observerOrder = new ObserverOrder();
         observerOrder.addObserver(message -> System.out.println("Notification: " + message));
         observerOrder.notifyTheUsers("Your order has been processed!");
